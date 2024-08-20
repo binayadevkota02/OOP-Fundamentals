e5d41abe102a7dbf266949dfd327edc56b33a176
binaya02 <binayadevkota@gmail.com>
2024-08-20 09:43:00 +0545

    assignment 5

---
+++ Assignment/Assignment5/Program.cs
@@ -0,0 +1,5 @@
+﻿string filePath = @"C:\Users\binaya.raj_teksewa\Desktop\OOP fundamental\OOP-Fundamentals\Assignment\Assignment5\";
+List<Person> people = await CSVParser.ParseCSV(filePath);
+await PeopleReport.SaveMales(people, filePath);
+await PeopleReport.SaveFemales(people, filePath);
+await PeopleReport.SaveDotComUsers(people, filePath);
